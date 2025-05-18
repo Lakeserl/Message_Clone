7ad44730a972cdc7fac46b11d40459806ded5c33
Lakeserl <dinhlam18112003@gmail.com>
2025-05-18 22:57:13 +0700

    Updated

---
+++ client/src/libs/utils.js
@@ -0,0 +1,7 @@
+export function formatMessengerTime(date){
+    return new Date(date).toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+    })
+}
