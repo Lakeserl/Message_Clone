7bb7ec6da8ecd11fdbd9b9676cfa520ea5b57f2b
Lakeserl <dinhlam18112003@gmail.com>
2025-05-21 23:49:21 +0700

    Updated: server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-import express from "express";
-import "dotenv/config";
 import cors from "cors";
+import "dotenv/config";
+import express from "express";
 import http from "http";
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 //Middleware setup
-app.use(express.json({limit: "4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
-app.use("/api/status", (req, res)=> res.send("Server is live"));
+app.use("/api/status", (req, res) => res.send("Server is live"));
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT )
+server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
